ce9289fb41562e3fdffa10b7e5d3fabcc7caa01b
Гушко Игорь <gigoralx@gmail.com>
2020-11-05 13:25:06 +0300

    CAT IS 2.11.20

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat("Петя", new DateTime(2007, 7, 13));
+            c.MakeNoise();
+            Console.WriteLine($"Коту {c.Name} уже {c.GetAge()} лет");
+        }
+    }
+
+}
---
+++ ConsoleApp3/cat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+   
+   
+        public class Cat
+        {
+            public Cat(string name, DateTime berthday)
+            {
+                Name = name;
+                BerthDay = berthday;
+            }
+
+            public string Name
+            {
+                get;
+                set;
+            }
+            public void MakeNoise()
+            {
+
+                Console.WriteLine($"{Name} мяукает");
+            }
+            public DateTime BerthDay
+            {
+                get;
+                set;
+            }
+            public int GetAge()
+            {
+                return (DateTime.Today - BerthDay).Days / 365;
+
+            }
+        }
+    }
+
+
